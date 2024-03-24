853a7ec2026f21d898099fb1c7facda2f67e87be
sergio-usma <sergioandresusma@hotmail.com>
2024-03-24 08:26:34 -0500

    Add ageMoodChecker exercise

---
+++ oldMoodChecker.py
@@ -0,0 +1,12 @@
+print('\nHi!👋 Check your generational age range!!')
+
+age = int(input('\nPlease insert your age!: '))
+
+if 0 <= age < 10:
+    print('\nThe childhood it\'s incredible!!🧒')
+elif 10 <= age < 20:
+    print('\nMany changes and many study!!🧑‍🎓')
+elif 20 <= age <= 30:
+    print('\nThe love and work starts!💞')
+else:
+    print('\nKeep walking!!😉')
