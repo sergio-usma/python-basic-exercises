adfe9d0fe8820123a0b0993f1f180f90e7525656
sergio-usma <sergioandresusma@hotmail.com>
2024-03-23 20:36:32 -0500

    Add checkGreaterNumber exercise!🕺

---
+++ checkGreaterNumber.py
@@ -0,0 +1,9 @@
+print('\nHi!👋 Check to the greater number!')
+
+number1 = int(input('\nInsert the first number: '))
+number2 = int(input('Insert the second number: '))
+
+if number1 > number2:
+    print(f'\nThe number {number1} is greater than {number2}!!😉')
+else:
+    print(f'\nThe number {number2} is greater than {number1}!!😉')
