4ef905a8d72e9f1329546123d2355e2f4f1f4134
sergio-usma <sergioandresusma@hotmail.com>
2024-03-23 21:00:28 -0500

    Add bookInventory exercise!🕺

--- bookInventory.py
+++ bookInventory.py
@@ -1,12 +1,12 @@
 print('\nHi!👋 Welcome to the Library!')
 
-print('\nInsert the book\'s information!')
+print('\nInsert the book\'s information!😉')
 
-bookName = input('\nInsert the book\'s name: ')
-bookAuthor = input('Insert the book\'s author: ')
-bookID = int(input('Insert the book\'s ID: '))
-bookPrice = float(input('Insert the book\'s price: '))
-freeShip = input('Free ship included? (yes/no:) ').lower()
+bookName = input('\n1️⃣ Insert the book\'s name: ')
+bookAuthor = input('2️⃣ Insert the book\'s author: ')
+bookID = int(input('3️⃣ Insert the book\'s ID: '))
+bookPrice = float(input('4️⃣ Insert the book\'s price: '))
+freeShip = input('5️⃣ Free ship included? (yes/no:) ').lower()
 
 if freeShip == 'yes':
     freeShipmentN = True
@@ -14,9 +14,10 @@ else:
     freeShipmentN = False
 
 print(f'''
-Book name: {bookName}
-Book author: {bookAuthor}
-ID: {bookID}
-Price: {bookPrice}
-Free ship? {freeShipmentN}
+This is the Book's information:📖
+1️⃣ Book name: {bookName}
+2️⃣ Book author: {bookAuthor}
+3️⃣ ID: {bookID}
+4️⃣ Price: {bookPrice}
+5️⃣ Free ship?: {freeShipmentN}
 ''')
