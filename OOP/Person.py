631e65f3c232a3ddc8772c32c459dfb93700a093
sergio-usma <sergioandresusma@hotmail.com>
2024-03-31 07:20:27 -0500

    Add advanced attributes for Person Class

--- OOP/Person.py
+++ OOP/Person.py
@@ -1,18 +1,20 @@
 class Person:
-    def __init__(self, name, lastName, age):
+    def __init__(self, name, lastName, age, *info, **preferences):
         self.name = name
         self.last_name = lastName
         self.age = age
+        self.info = info
+        self.preferences = preferences
 
     def show_detail(self):
-        print(f'Persona: {self.name} {self.last_name} {self.age}')
+        print(f'Persona: {self.name} {self.last_name} {self.age} {self.info} {self.preferences}')
 
 
-person1 = Person('John', 'Doe', 29)
+person1 = Person('John', 'Doe', 29, 'Married', 'Lawyer', 'Law&Order inc.', spouse='Whiskas Lopez', children=3, born='05-03-1995')
 person1.show_detail()
-person1.telephone = '42313'
+print(person1.info[1])
+print(person1.preferences['spouse'])
 # Person.show_detail(person1)
-print(person1.telephone)
 
 person2 = Person('Jane', 'Doe', 33)
 person2.show_detail()
