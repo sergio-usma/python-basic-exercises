483afca44a940e5096b907aafed1befc874a3ccf
sergio-usma <sergioandresusma@hotmail.com>
2024-03-30 22:57:51 -0500

    Practice OOP w/ basic commands

--- OOP/Person.py
+++ OOP/Person.py
@@ -1,20 +1,21 @@
 class Person:
     def __init__(self, name, lastName, age):
         self.name = name
-        self.lastName = lastName
+        self.last_name = lastName
         self.age = age
 
-
-nombre = input('Insert your name: ')
-apellido = input('Insert your last name: ')
-edad = int(input('Insert your age: '))
-
-person1 = Person(nombre, apellido, edad)
-print(person1.name, end=' ')
-print(person1.lastName)
-print(person1.age)
+    def show_detail(self):
+        print(f'Persona: {self.name} {self.last_name} {self.age}')
 
 
+person1 = Person('John', 'Doe', 29)
+person1.show_detail()
+person1.telephone = '42313'
+# Person.show_detail(person1)
+print(person1.telephone)
+
+person2 = Person('Jane', 'Doe', 33)
+person2.show_detail()
 
 
 
